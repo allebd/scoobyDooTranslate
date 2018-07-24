210258f95382c57bd805f448843b89bed9220760
allebd <delebella@yahoo.com>
2018-07-24 08:40:57 +0100

    All scooby files

---
+++ scooby.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+  //Assign variables
+  const translateForm = $("#translateForm");
+  const myWord = $("#myWord");
+  const scoobyTranslation = $("#scoobyTranslation");
+
+  //Function to translate on submit
+  translateForm.submit(function(event) {
+    event.preventDefault();
+    let myWordVal = myWord.val();
+    makeTranslation(myWordVal);
+  });
+
+  //Function to make translation
+  function makeTranslation(myWordVal) {
+    let vowels = ["a", "e", "i", "o", "u"]; //Array of Vowels
+    let scoobyWord = []; //Stores Scooby's Words
+    let theWord = myWordVal.trim();
+    if (theWord === "") {
+      displayTranslation(theWord);
+    } else {
+      let allWords = theWord.toLowerCase().split(" ");
+      for (let i = 0; i < allWords.length; i++) {
+        let eachWord = allWords[i].split("");
+        let eachWordLength = eachWord.length;
+        let theCounter = 0;
+        innerLoop: for (let j = 0; j < eachWordLength; j++) {
+          let firstCharacter = eachWord[0];
+          if (vowels.includes(firstCharacter)) {
+            scoobyWord.push(eachWord.join(""));
+            break innerLoop;
+          } else {
+            let eachCharacter = eachWord[j];
+            if (vowels.includes(eachCharacter)) {
+              theCounter++;
+              if (theCounter === 1) {
+                let newWord = "r" + eachWord.slice(j).join("");
+                scoobyWord.push(newWord);
+                break innerLoop;
+              }
+            }
+            if (j + 1 === eachWordLength) {
+              scoobyWord.push("r");
+              break innerLoop;
+            }
+          }
+        }
+      }
+      displayTranslation(scoobyWord);
+    }
+  }
+
+  //function to display translation
+  function displayTranslation(theWord) {
+    if (theWord === "") {
+      scoobyTranslation.val("");
+    } else {
+      let scoobyWords = theWord.join(" ");
+      scoobyTranslation.val(scoobyWords);
+    }
+  }
+});
