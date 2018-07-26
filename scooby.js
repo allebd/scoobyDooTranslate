366d9d5d46a8275bdb49cdf6bbea1f3b19b518da
allebd <delebella@yahoo.com>
2018-07-26 07:56:35 +0100

    Update script

--- scooby.js
+++ scooby.js
@@ -1,62 +1,60 @@
-$(document).ready(function() {
-  //Assign variables
-  const translateForm = $("#translateForm");
-  const myWord = $("#myWord");
-  const scoobyTranslation = $("#scoobyTranslation");
+//Assign variables
+const translateForm = document.getElementById("translateForm");
+const myWord = document.getElementById("myWord");
+const scoobyTranslation = document.getElementById("scoobyTranslation");
 
-  //Function to translate on submit
-  translateForm.submit(function(event) {
-    event.preventDefault();
-    let myWordVal = myWord.val();
-    makeTranslation(myWordVal);
-  });
+//Function to translate on submit
+translateForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  let myWordVal = myWord.value;
+  makeTranslation(myWordVal);
+});
 
-  //Function to make translation
-  function makeTranslation(myWordVal) {
-    let vowels = ["a", "e", "i", "o", "u"]; //Array of Vowels
-    let scoobyWord = []; //Stores Scooby's Words
-    let theWord = myWordVal.trim();
-    if (theWord === "") {
-      displayTranslation(theWord);
-    } else {
-      let allWords = theWord.toLowerCase().split(" ");
-      for (let i = 0; i < allWords.length; i++) {
-        let eachWord = allWords[i].split("");
-        let eachWordLength = eachWord.length;
-        let theCounter = 0;
-        innerLoop: for (let j = 0; j < eachWordLength; j++) {
-          let firstCharacter = eachWord[0];
-          if (vowels.includes(firstCharacter)) {
-            scoobyWord.push(eachWord.join(""));
-            break innerLoop;
-          } else {
-            let eachCharacter = eachWord[j];
-            if (vowels.includes(eachCharacter)) {
-              theCounter++;
-              if (theCounter === 1) {
-                let newWord = "r" + eachWord.slice(j).join("");
-                scoobyWord.push(newWord);
-                break innerLoop;
-              }
+//Function to make translation
+function makeTranslation(myWordVal) {
+  let vowels = ["a", "e", "i", "o", "u"]; //Array of Vowels
+  let scoobyWord = []; //Stores Scooby's Words
+  let theWord = myWordVal.trim();
+  if (theWord === "") {
+    displayTranslation(theWord);
+  } else {
+    let allWords = theWord.toLowerCase().split(" ");
+    for (let i = 0; i < allWords.length; i++) {
+      let eachWord = allWords[i].split("");
+      let eachWordLength = eachWord.length;
+      let theCounter = 0;
+      for (let j = 0; j < eachWordLength; j++) {
+        let firstCharacter = eachWord[0];
+        if (vowels.includes(firstCharacter)) {
+          scoobyWord.push(eachWord.join(""));
+          break;
+        } else {
+          let eachCharacter = eachWord[j];
+          if (vowels.includes(eachCharacter)) {
+            theCounter++;
+            if (theCounter === 1) {
+              let newWord = "r" + eachWord.slice(j).join("");
+              scoobyWord.push(newWord);
+              break;
             }
-            if (j + 1 === eachWordLength) {
-              scoobyWord.push("r");
-              break innerLoop;
-            }
+          }
+          if (j + 1 === eachWordLength) {
+            scoobyWord.push("r");
+            break;
           }
         }
       }
-      displayTranslation(scoobyWord);
     }
+    displayTranslation(scoobyWord);
   }
+}
 
-  //function to display translation
-  function displayTranslation(theWord) {
-    if (theWord === "") {
-      scoobyTranslation.val("");
-    } else {
-      let scoobyWords = theWord.join(" ");
-      scoobyTranslation.val(scoobyWords);
-    }
+//function to display translation
+function displayTranslation(theWord) {
+  if (theWord === "") {
+    scoobyTranslation.value = "No text inputted.";
+  } else {
+    let scoobyWords = theWord.join(" ");
+    scoobyTranslation.value = scoobyWords;
   }
-});
+}
